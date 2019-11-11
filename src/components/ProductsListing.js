325dbe307b1960ad5ce91e57e64230b76a9dad50
Stephan <sgriesel@gmail.com>
2019-11-11 06:09:38 +0100

    add product, buggy version

--- src/components/ProductsListing.js
+++ src/components/ProductsListing.js
@@ -1,32 +1,33 @@
 import React from 'react';
 import { useStaticQuery, graphql } from "gatsby";
+import Product from './Product';
 
 const PRODUCTS_LISTING_QUERY = graphql`
 query ProductsListingQuery{
-    allShopifyProduct {
-        edges {
-          node {
-            id
-            title
-            publishedAt(formatString: "YYYY")
-            description
-            descriptionHtml
-            variants {
-              sku
-              id
-              title
-              price
-            }
-          }
+  allShopifyProduct {
+    edges {
+      node {
+        id
+        title
+        description
+        descriptionHtml
+        variants {
+          sku
         }
       }
+    }
+  }
 }
 `;
 
 const ProductsListing = () => {
-    const data = useStaticQuery(PRODUCTS_LISTING_QUERY)
-    console.log("data", data);
-    return <div></div>
+  const { allShopifyProduct } = useStaticQuery(PRODUCTS_LISTING_QUERY);
+  return
+  (<div>
+    {allShopifyProduct.edges.map(edge => (
+      <Product product={edge.node} key={edge.node.id} />
+    ))}
+  </div>)
 }
 
 export default ProductsListing;
