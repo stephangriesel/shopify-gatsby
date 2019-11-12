2c24996695160ab6435cfaa3a8cb243330cfae1f
Stephan <sgriesel@gmail.com>
2019-11-12 05:57:53 +0100

    pdate & map over title

--- src/components/ProductsListing.js
+++ src/components/ProductsListing.js
@@ -1,33 +1,37 @@
-import React from 'react';
-import { useStaticQuery, graphql } from "gatsby";
-import Product from './Product';
+import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Product from './Product'
 
 const PRODUCTS_LISTING_QUERY = graphql`
-query ProductsListingQuery{
-  allShopifyProduct {
-    edges {
-      node {
-        id
-        title
-        description
-        descriptionHtml
-        variants {
-          sku
+  query ProductsListingQuery {
+    allShopifyProduct {
+      edges {
+        node {
+          id
+          title
+          publishedAt(formatString: "YYYY")
+          description
+          descriptionHtml
+          variants {
+            sku
+            id
+            title
+            price
+          }
         }
       }
     }
   }
-}
-`;
+`
 
 const ProductsListing = () => {
-  const { allShopifyProduct } = useStaticQuery(PRODUCTS_LISTING_QUERY);
-  return
-  (<div>
+  const { allShopifyProduct } = useStaticQuery(PRODUCTS_LISTING_QUERY)
+  // console.log("data", data)
+  return <div>
     {allShopifyProduct.edges.map(edge => (
       <Product product={edge.node} key={edge.node.id} />
     ))}
-  </div>)
+  </div>
 }
 
-export default ProductsListing;
+export default ProductsListing
