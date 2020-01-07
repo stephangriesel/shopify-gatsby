176a8bf6b617ba4369df61e102789c12712ef0d8
Stephan <sgriesel@gmail.com>
2020-01-07 06:33:04 +0100

    add query for pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,31 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require('path');
 
-// You can delete this file if you're not using it
+exports.createPages = async ({
+    graphql,
+    actions: { createPage }
+}) => {
+    const pages = await graphql(`
+    {
+        allShopifyProduct {
+          edges {
+            node {
+              id
+              handle
+            }
+          }
+        }
+      }
+      
+    `);
+
+    pages.data.allShopifyProduct.edges.forEach(edge => {
+        createPage({
+            path: `/products/${edge.node.handle}`,
+            component: path.resolve('./src/pages/page-2.js'),
+            context: {
+                id: edge.node.id,
+                handle: edge.node.handle
+            },
+        })
+    })
+}
